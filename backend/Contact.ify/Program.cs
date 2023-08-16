0aec9eddb339d441c2d0dd89371618b55e536513
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-16 21:41:28 +0800

    add authentication to di container and middleware

--- backend/Contact.ify/Program.cs
+++ backend/Contact.ify/Program.cs
@@ -1,13 +1,17 @@
+using System.Text;
+using System.Text.Json.Serialization;
 using Contact.ify.DataAccess.Data;
 using Contact.ify.DataAccess.UnitOfWork;
 using Contact.ify.Domain.Services;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+builder.Services.AddControllers()
+    .AddJsonOptions(option => option.JsonSerializerOptions.ReferenceHandler = ReferenceHandler.IgnoreCycles);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -16,6 +20,24 @@ builder.Services.AddDbContext<ContactsContext>(options =>
     options.UseSqlServer(
         builder.Configuration.GetConnectionString("ContactsDb"))
 );
+builder.Services
+    .AddAuthentication("Bearer")
+    .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateIssuerSigningKey = true,
+                ValidateLifetime = true,
+                
+                ValidIssuer = builder.Configuration.GetSection("Authentication:Issuer").Value,
+                ValidAudience = builder.Configuration.GetSection("Authentication:Audience").Value,
+                IssuerSigningKey =new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(builder.Configuration.GetSection("Authentication:SecretKey").Value!))
+            };
+        }
+    );
 
 builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 builder.Services.AddScoped<IUsersService, UsersService>();
@@ -31,6 +53,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
