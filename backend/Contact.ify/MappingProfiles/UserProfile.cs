585f12c1b60da0384deabbcb7dbeec2483f9cb3a
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-16 12:25:05 +0800

    add mapping profile for users
    I also added automapper to the di container

---
+++ backend/Contact.ify/MappingProfiles/UserProfile.cs
@@ -0,0 +1,23 @@
+using AutoMapper;
+using Contact.ify.DataAccess.Entities;
+using Contact.ify.Domain.DTOs;
+
+namespace Contact.ify.MappingProfiles;
+
+public class UserProfile : Profile
+{
+    public UserProfile()
+    {
+        CreateMap<RegisterUserRequest, User>()
+            .ForMember(dest => dest.PasswordHash,
+                opt => opt.MapFrom( request =>
+                    BCrypt.Net.BCrypt.HashPassword(request.Password) ))
+            .ConstructUsing(src => 
+                new User(
+                    src.UserName, 
+                    string.Empty, 
+                    src.FirstName, 
+                    src.LastName, 
+                    src.Email));
+    }
+}
