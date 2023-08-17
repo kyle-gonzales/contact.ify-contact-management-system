ff7b4aa1a40ca839af06a76132f7b657756feb7b
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-17 11:45:47 +0800

    add logging to AuthenticationController.cs

--- backend/Contact.ify/Controllers/AuthenticationController.cs
+++ backend/Contact.ify/Controllers/AuthenticationController.cs
@@ -9,37 +9,64 @@ namespace Contact.ify.Controllers;
 public class AuthenticationController : ControllerBase
 {
     private readonly IUsersService _usersService;
+    private readonly ILogger<AuthenticationController> _logger;
 
-    public AuthenticationController(IUsersService usersService)
+    public AuthenticationController(IUsersService usersService, ILogger<AuthenticationController> logger)
     {
         _usersService = usersService ?? throw new ArgumentNullException(nameof(usersService));
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
     }
 
     [HttpPost]
     [Route("/register")]
     public async Task<IActionResult> Register([FromBody] RegisterUserRequest request)
     {
-        var isSuccess = await _usersService.RegisterUserAsync(request);
-
-        if (isSuccess)
+        try
         {
-            return Ok();
-        }
+            var isSuccess = await _usersService.RegisterUserAsync(request);
+            if (isSuccess)
+            {
+                return Ok();
+            }
 
-        return BadRequest("Something went wrong when trying to register your account");
+            return BadRequest("Something went wrong when trying to register your account");
+        }
+        catch (OperationCanceledException e)
+        {
+            _logger.LogError(e, "Database operation was cancelled");
+            return StatusCode(StatusCodes.Status500InternalServerError, "The database provider is currently unavailable.");
+        }
+        catch (Exception)
+        {
+            _logger.LogError("Exception occurred when trying to register a user");
+            throw;
+        }
     }
 
     [HttpPost]
     [Route("/login")]
     public async Task<ActionResult<string>> Login([FromBody] LoginUserRequest request)
     {
-        var token = await _usersService.LoginUserAsync(request);
-
-        if (token == null)
+        try
         {
-            return BadRequest("Something went wrong when trying to log in");
+            var token = await _usersService.LoginUserAsync(request);
+
+            if (token == null)
+            {
+                return BadRequest("Something went wrong when trying to log in");
+            }
+            return Ok(token);
         }
-        return Ok(token);
+        catch (OperationCanceledException e)
+        {
+            _logger.LogError(e, "Database operation was cancelled");
+            return StatusCode(StatusCodes.Status500InternalServerError, "The database provider is currently unavailable.");
+        }
+        catch (Exception)
+        {
+            _logger.LogError("Exception occurred when trying to login");
+            throw;
+        }
     }
 }
 
