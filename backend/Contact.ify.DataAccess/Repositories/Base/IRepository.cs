a9c50633b8adc18885218d59e08715e0a1a90f26
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-18 17:15:26 +0800

    remove files for the generic repository pattern
    I decided to not use the generic repository pattern because my business requirements do not jive with the pattern.

--- backend/Contact.ify.DataAccess/Repositories/Base/IRepository.cs
+++
@@ -1,8 +0,0 @@
-namespace Contact.ify.DataAccess.Repositories;
-
-public interface IRepository<TEntity> where TEntity : class
-{
-    void Add(TEntity entity);
-    Task<TEntity?> GetAsync(int id);
-    Task<ICollection<TEntity>> GetAllAsync();
-}
