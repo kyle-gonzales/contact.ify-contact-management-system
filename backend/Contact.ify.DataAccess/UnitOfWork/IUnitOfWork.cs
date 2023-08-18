e724253f60941c133372f98cd998e8c5657ed35b
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-18 17:16:35 +0800

    updated the unit of work with the new repositories

--- backend/Contact.ify.DataAccess/UnitOfWork/IUnitOfWork.cs
+++ backend/Contact.ify.DataAccess/UnitOfWork/IUnitOfWork.cs
@@ -1,3 +1,8 @@
+using Contact.ify.DataAccess.Repositories.Addresses;
+using Contact.ify.DataAccess.Repositories.ContactAuditTrail;
+using Contact.ify.DataAccess.Repositories.Contacts;
+using Contact.ify.DataAccess.Repositories.Emails;
+using Contact.ify.DataAccess.Repositories.PhoneNumbers;
 using Contact.ify.DataAccess.Repositories.Users;
 
 namespace Contact.ify.DataAccess.UnitOfWork;
@@ -5,5 +10,13 @@ namespace Contact.ify.DataAccess.UnitOfWork;
 public interface IUnitOfWork : IDisposable
 {
     IUsersRepository Users { get; }
+    
+    IContactsRepository Contacts { get;  }
+    IPhoneNumbersRepository PhoneNumbers { get;  }
+    IAddressesRepository Addresses { get;  }
+    IEmailsRepository Emails { get; }
+    
+    IContactAuditTrailRepository AuditTrail { get; }
+    
     Task CompleteAsync();
 }
--- backend/Contact.ify.DataAccess/UnitOfWork/UnitOfWork.cs
+++ backend/Contact.ify.DataAccess/UnitOfWork/UnitOfWork.cs
@@ -1,5 +1,10 @@
 using System.Data;
 using Contact.ify.DataAccess.Data;
+using Contact.ify.DataAccess.Repositories.Addresses;
+using Contact.ify.DataAccess.Repositories.ContactAuditTrail;
+using Contact.ify.DataAccess.Repositories.Contacts;
+using Contact.ify.DataAccess.Repositories.Emails;
+using Contact.ify.DataAccess.Repositories.PhoneNumbers;
 using Contact.ify.DataAccess.Repositories.Users;
 
 namespace Contact.ify.DataAccess.UnitOfWork;
@@ -10,10 +15,23 @@ public class UnitOfWork : IUnitOfWork
     private readonly ContactsContext _context;
 
     public IUsersRepository Users { get; }
+    public IContactsRepository Contacts { get; }
+    public IPhoneNumbersRepository PhoneNumbers { get; }
+    public IAddressesRepository Addresses { get; }
+    public IEmailsRepository Emails { get; }
+    public IContactAuditTrailRepository AuditTrail { get; }
+
     public UnitOfWork(ContactsContext context)
     {
         _context = context ?? throw new NoNullAllowedException(nameof(_context));
         Users = new UsersRepository(_context);
+
+        Contacts = new ContactsRepository(_context);
+        PhoneNumbers = new PhoneNumbersRepository(_context);
+        Addresses = new AddressesRepository(_context);
+        Emails = new EmailsRepository(_context);
+
+        AuditTrail = new ContactAuditTrailRepository(_context);
     }
     
     public async Task CompleteAsync()
