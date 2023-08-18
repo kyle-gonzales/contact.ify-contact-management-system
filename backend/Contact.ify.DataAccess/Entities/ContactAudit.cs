f81910f23f57a5fc01df64ac1b1de9147373d6d9
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-18 17:09:21 +0800

    add audit entity

--- backend/Contact.ify.DataAccess/Entities/ContactAudit.cs
+++ backend/Contact.ify.DataAccess/Entities/ContactAudit.cs
@@ -1,6 +1,28 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Contact.ify.DataAccess.Entities;
 
 public class ContactAudit
 {
+    public int ContactAuditId { get; set; }
     
+    [Required]
+    public DateTimeOffset Timestamp { get; set; }
+    
+    [Required]
+    public int ContactId { get; set; } // not a foreign key
+    
+    [Required]
+    public int ModifiedBy { get; set; } // person who modified
+    
+    [Required]
+    public ModificationType ModificationType { get; set; }
+
+}
+
+public enum ModificationType
+{
+    Create = 1,
+    Update = 2,
+    Delete = 3
 }
