454894aa07d3d6f801ab78e1435c6983c7fee98c
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-18 17:09:00 +0800

    add contact entity

--- backend/Contact.ify.DataAccess/Entities/Contact.cs
+++ backend/Contact.ify.DataAccess/Entities/Contact.cs
@@ -1,6 +1,46 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Contact.ify.DataAccess.Entities;
 
 public class Contact
 {
+    public int ContactId { get; set; }
+
+    [Required]
+    [MinLength(2)]
+    [MaxLength(15)]
+    public string UserId { get; set; } = null!;
     
+    [MinLength(2)]
+    [MaxLength(35)]
+    public string? FirstName { get; set; }
+    
+    [Required]
+    [MinLength(2)]
+    [MaxLength(35)]
+    public string LastName { get; set; }
+
+    public List<ContactAddress> Addresses { get; set; }
+    public List<ContactPhoneNumber> PhoneNumbers { get; set; }
+    public List<ContactEmail> Emails { get; set; }
+    
+    public bool IsFavorite { get; set; }
+    
+    public bool IsDeleted { get; set; }
+    public DateTimeOffset LastDateModified { get; set; }
+
+    public Contact(
+        string lastName,
+        DateTimeOffset lastDateModified,
+        string? firstName = null
+    )
+    {
+        LastName = lastName;
+        LastDateModified = lastDateModified;
+        FirstName = firstName;
+        
+        Addresses = new List<ContactAddress>();
+        PhoneNumbers = new List<ContactPhoneNumber>();
+        Emails = new List<ContactEmail>();
+    }
 }
