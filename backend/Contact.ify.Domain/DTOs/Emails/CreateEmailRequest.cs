94c8461c65577be3b98bab02e3a097424c7a08e4
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-22 11:20:23 +0800

    update CreateEmailRequest.cs DTO
    I removed the constructor because AutoMapper uses the parameterless constructor.

--- backend/Contact.ify.Domain/DTOs/Emails/CreateEmailRequest.cs
+++ backend/Contact.ify.Domain/DTOs/Emails/CreateEmailRequest.cs
@@ -7,10 +7,5 @@ public class CreateEmailRequest
     [EmailAddress(ErrorMessage = "Email is in an invalid format")]
     [MinLength(4, ErrorMessage = "Email must be at least 4 characters long")]
     [MaxLength(255, ErrorMessage = "Email must not be longer than 255 characters")]
-    public string Email { get; set; }
-
-    public CreateEmailRequest(string email)
-    {
-        Email = email;
-    }
+    public string Email { get; set; } = string.Empty;
 }
