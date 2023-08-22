a301f8cc5a545cdc83981c9ee3a099ac94e468f5
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-22 11:32:44 +0800

    update UserResponse.cs DTO
    I removed the constructor because AutoMapper uses the parameterless constructor.

--- backend/Contact.ify.Domain/DTOs/Users/UserResponse.cs
+++ backend/Contact.ify.Domain/DTOs/Users/UserResponse.cs
@@ -2,24 +2,11 @@ namespace Contact.ify.Domain.DTOs.Users;
 
 public class UserResponse
 {
-    public string UserName { get; set; }
+    public string UserName { get; set; } = string.Empty;
     
     public string? FirstName { get; set; }
     
     public string? LastName { get; set; }
     
     public string? Email { get; set; }
-
-    public UserResponse(
-        string userName,
-        string? firstName = null,
-        string? lastName = null,
-        string? email = null
-    )
-    {
-        UserName = userName;
-        FirstName = firstName;
-        LastName = lastName;
-        Email = email;
-    }
 }
