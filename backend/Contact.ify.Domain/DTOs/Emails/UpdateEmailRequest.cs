96ce024fa67c449999af7feeb7441c9796ff144d
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-22 11:22:05 +0800

    update UpdateEmailRequest.cs DTO
    I removed the constructor because AutoMapper uses the parameterless constructor.

--- backend/Contact.ify.Domain/DTOs/Emails/UpdateEmailRequest.cs
+++ backend/Contact.ify.Domain/DTOs/Emails/UpdateEmailRequest.cs
@@ -4,17 +4,11 @@ namespace Contact.ify.Domain.DTOs.Emails;
 
 public class UpdateEmailRequest
 {
-
     [Required]
     public int ContactEmailId { get; set; }
     
     [EmailAddress(ErrorMessage = "Email is in an invalid format")]
     [MinLength(4, ErrorMessage = "Email must be at least 4 characters long")]
     [MaxLength(255, ErrorMessage = "Email must not be longer than 255 characters")]
-    public string Email { get; set; }
-    
-    public UpdateEmailRequest(string email)
-    {
-        Email = email;
-    }
+    public string Email { get; set; } = string.Empty;
 }
