b53205b5535b62ff910b378d99f18a3edfbdd883
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-22 11:45:34 +0800

    remove constructor in ContactEmail.cs
    This is because AutoMapper uses the
    default parameterless constructor.

--- backend/Contact.ify.DataAccess/Entities/ContactEmail.cs
+++ backend/Contact.ify.DataAccess/Entities/ContactEmail.cs
@@ -8,16 +8,11 @@ public class ContactEmail
     public int ContactEmailId { get; set; }
 
     [Required] [ForeignKey("ContactId")] public Contact Contact { get; set; } = null!;
-    
+
     [EmailAddress]
     [MinLength(4)]
     [MaxLength(255)]
-    public string Email { get; set; }
+    public string Email { get; set; } = string.Empty;
 
     [Required] public bool IsDeleted { get; set; }
-
-    public ContactEmail(string email)
-    {
-        Email = email;
-    }
 }
