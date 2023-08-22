4ff66d924caf2cf4df8346e1a749be6302c91c10
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-22 11:21:34 +0800

    update EmailResponse.cs DTO
    added ContactId property.
    
    I removed the constructor because AutoMapper uses the parameterless constructor.

--- backend/Contact.ify.Domain/DTOs/Emails/EmailResponse.cs
+++ backend/Contact.ify.Domain/DTOs/Emails/EmailResponse.cs
@@ -2,11 +2,7 @@ namespace Contact.ify.Domain.DTOs.Emails;
 
 public class EmailResponse
 {
+    public int ContactId { get; set; }
     public int ContactEmailId { get; set; }
-    public string Email { get; set; }
-
-    public EmailResponse(string email)
-    {
-        Email = email;
-    }
+    public string Email { get; set; } = string.Empty;
 }
