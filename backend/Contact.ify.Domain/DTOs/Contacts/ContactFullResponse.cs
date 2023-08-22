3cc739da1e0d40ace533b2b6fe2e0d93ea63547f
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-22 11:16:48 +0800

    update ContactFullResponse.cs DTO
    I removed the constructor because AutoMapper uses the parameterless constructor.

--- backend/Contact.ify.Domain/DTOs/Contacts/ContactFullResponse.cs
+++ backend/Contact.ify.Domain/DTOs/Contacts/ContactFullResponse.cs
@@ -8,21 +8,12 @@ public class ContactFullResponse
 {
     public int ContactId { get; set; } 
     public string? FirstName { get; set; }
-    public string LastName { get; set; }
+    public string LastName { get; set; } = string.Empty;
     
     public bool IsFavorite { get; set; }
-    
-    public List<EmailResponse> Emails { get; set; }
-    public List<PhoneNumberResponse> PhoneNumbers { get; set; }
-    public List<AddressResponse> Addresses { get; set; }
 
-    public ContactFullResponse(string? firstName, string lastName)
-    {
-        FirstName = firstName;
-        LastName = lastName;
+    public List<EmailResponse> Emails { get; set; } = new();
+    public List<PhoneNumberResponse> PhoneNumbers { get; set; } = new();
+    public List<AddressResponse> Addresses { get; set; } = new();
 
-        Emails = new List<EmailResponse>();
-        PhoneNumbers = new List<PhoneNumberResponse>();
-        Addresses = new List<AddressResponse>();
-    }
 }
