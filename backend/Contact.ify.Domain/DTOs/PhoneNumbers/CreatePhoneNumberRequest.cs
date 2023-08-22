fc73d48d9b48cb24d71553d42670f5b790f7f81a
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-22 11:22:37 +0800

    update CreatePhoneNumberRequest.cs DTO
    I removed the constructor because AutoMapper uses the parameterless constructor.

--- backend/Contact.ify.Domain/DTOs/PhoneNumbers/CreatePhoneNumberRequest.cs
+++ backend/Contact.ify.Domain/DTOs/PhoneNumbers/CreatePhoneNumberRequest.cs
@@ -4,13 +4,7 @@ namespace Contact.ify.Domain.DTOs.PhoneNumbers;
 
 public class CreatePhoneNumberRequest
 {
-
     [Phone(ErrorMessage = "Phone Number is in an invalid format")]
     [MaxLength(20, ErrorMessage = "Phone Number cannot be longer than 20 characters")]
-    public string PhoneNumber { get; set; }
-    
-    public CreatePhoneNumberRequest(string phoneNumber)
-    {
-        PhoneNumber = phoneNumber;
-    }
+    public string PhoneNumber { get; set; } = string.Empty;
 }
