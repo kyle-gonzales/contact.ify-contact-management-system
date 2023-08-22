cca37c5d78ce4456ca65972bba4e63ce5f817682
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-22 11:37:15 +0800

    implement addresses service

--- backend/Contact.ify.Domain/Services/Addresses/AddressesService.cs
+++ backend/Contact.ify.Domain/Services/Addresses/AddressesService.cs
@@ -1,7 +1,7 @@
 using AutoMapper;
 using Contact.ify.DataAccess.Entities;
 using Contact.ify.DataAccess.UnitOfWork;
-using Microsoft.Extensions.Configuration;
+using Contact.ify.Domain.DTOs.Addresses;
 
 namespace Contact.ify.Domain.Services.Addresses;
 
@@ -9,38 +9,81 @@ public class AddressesService : IAddressesService
 {
     private readonly IUnitOfWork _unitOfWork;
     private readonly IMapper _mapper;
-    private readonly IConfiguration _configuration;
 
-    public AddressesService(IUnitOfWork unitOfWork, IMapper mapper, IConfiguration configuration)
+    public AddressesService(IUnitOfWork unitOfWork, IMapper mapper)
     {
         _unitOfWork= unitOfWork ?? throw new ArgumentNullException(nameof(unitOfWork));
         _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
-        _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
     }
 
-
-    public Task<bool> AddAddress()
+    public async Task<int?> AddAddressForContactAsync(string userId, int contactId, CreateAddressRequest request)
     {
-        throw new NotImplementedException();
+        var contact = await _unitOfWork.Contacts.GetContactByIdForUserAsync(userId, contactId);
+        if (contact is null)
+        {
+            return null;
+        }
+        var address = _mapper.Map<ContactAddress>(request);
+        address.Contact = contact;
+        _unitOfWork.Addresses.AddAddress(address);
+        
+        contact.LastDateModified = DateTimeOffset.UtcNow;
+        
+        _unitOfWork.AuditTrail.Add(contact.ContactId, contact.UserId, ModificationType.Update, PropertyUpdated.Addresses);
+        await _unitOfWork.CompleteAsync();
+        return address.ContactAddressId;
     }
 
-    public Task<bool> UpdateAddress()
+    public async Task<bool> UpdateAddressForContactAsync(string userId, int contactId, UpdateAddressRequest request)
     {
-        throw new NotImplementedException();
+        var contact = await _unitOfWork.Contacts.GetContactByIdForUserAsync(userId, contactId);
+        if (contact is null)
+            return false;
+
+        var address = _mapper.Map<ContactAddress>(request);
+        address.Contact = contact;
+        await _unitOfWork.Addresses.UpdateAddressAsync(address);
+
+        contact.LastDateModified = DateTimeOffset.UtcNow;
+        
+        _unitOfWork.AuditTrail.Add(contact.ContactId, contact.UserId, ModificationType.Update, PropertyUpdated.Addresses);
+        await _unitOfWork.CompleteAsync();
+        return true;
     }
 
-    public Task<bool> RemoveAddress()
+    public async Task<bool> DeleteAddressForUserAsync(string userId, int contactId, int id)
     {
-        throw new NotImplementedException();
+        var contact = await _unitOfWork.Contacts.GetContactByIdForUserAsync(userId, contactId);
+        var address = await _unitOfWork.Addresses.GetAddressByIdForUserAsync(userId, id);
+
+        if (address is null || contact is null)
+            return false;
+        
+        _unitOfWork.Addresses.RemoveAddress(address);
+        
+        contact.LastDateModified = DateTimeOffset.UtcNow;
+        _unitOfWork.AuditTrail.Add(contact.ContactId, contact.UserId, ModificationType.Update, PropertyUpdated.Addresses);
+        await _unitOfWork.CompleteAsync();
+        return true;
     }
 
-    public async Task<ICollection<ContactAddress>?> GetAllAddressesForUserAsync(string userId)
+    public async Task<ICollection<AddressResponse>> GetAllAddressesForUserAsync(string userId)
     {
-        return await _unitOfWork.Addresses.GetAllAddressesForUserAsync(userId);
+        var addresses =  await _unitOfWork.Addresses.GetAllAddressesForUserAsync(userId);
+        var response = _mapper.Map<ICollection<ContactAddress>, ICollection<AddressResponse>>(addresses);
+
+        return response;
     }
 
-    public async Task<ContactAddress?> GetAddressByIdForUserAsync(string userId, int id)
+    public async Task<AddressResponse?> GetAddressByIdForUserAsync(string userId, int id)
     {
-        return await _unitOfWork.Addresses.GetAddressByIdForUserAsync(userId, id);
+        var address =  await _unitOfWork.Addresses.GetAddressByIdForUserAsync(userId, id);
+
+        if (address == null)
+        {
+            return null;
+        }
+        var response = _mapper.Map<ContactAddress, AddressResponse>(address);
+        return response;
     }
 }
--- backend/Contact.ify.Domain/Services/Addresses/IAddressesService.cs
+++ backend/Contact.ify.Domain/Services/Addresses/IAddressesService.cs
@@ -1,13 +1,13 @@
-using Contact.ify.DataAccess.Entities;
+using Contact.ify.Domain.DTOs.Addresses;
 
 namespace Contact.ify.Domain.Services.Addresses;
 
 public interface IAddressesService
 {
-    Task<bool> AddAddress();
-    Task<bool> UpdateAddress();
-    Task<bool> RemoveAddress();
+    Task<int?> AddAddressForContactAsync(string userId, int contactId, CreateAddressRequest request);
+    Task<bool> UpdateAddressForContactAsync(string userId, int contactId, UpdateAddressRequest request);
+    Task<bool> DeleteAddressForUserAsync(string userId, int contactId, int id);
     
-    Task<ICollection<ContactAddress>?> GetAllAddressesForUserAsync(string userId);
-    Task<ContactAddress?> GetAddressByIdForUserAsync(string userId, int id);
+    Task<ICollection<AddressResponse>> GetAllAddressesForUserAsync(string userId);
+    Task<AddressResponse?> GetAddressByIdForUserAsync(string userId, int id);
 }
