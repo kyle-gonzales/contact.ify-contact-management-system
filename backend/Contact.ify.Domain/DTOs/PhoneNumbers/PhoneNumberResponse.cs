15a1b4b9b02e8d9079ccb94ed6d71811da35eb42
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-22 11:31:45 +0800

    update PhoneNumberResponse.cs DTO
    added ContactId property
    
    I removed the constructor because AutoMapper uses the parameterless constructor

--- backend/Contact.ify.Domain/DTOs/PhoneNumbers/PhoneNumberResponse.cs
+++ backend/Contact.ify.Domain/DTOs/PhoneNumbers/PhoneNumberResponse.cs
@@ -4,11 +4,7 @@ namespace Contact.ify.Domain.DTOs.PhoneNumbers;
 
 public class PhoneNumberResponse
 {
+    public int ContactId { get; set; }
     public int ContactPhoneNumberId { get; set; }
-    public string PhoneNumber { get; set; }
-    
-    public PhoneNumberResponse(string phoneNumber)
-    {
-        PhoneNumber = phoneNumber;
-    }
+    public string PhoneNumber { get; set; } = string.Empty;
 }
