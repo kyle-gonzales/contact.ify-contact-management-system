471eca597139d50f4eaa28c6dc68dcf21e4dc6e5
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-22 11:19:10 +0800

    update CreateContactRequest.cs DTO
    added UserId property for simpler mapping to Contact entity
    
    I removed the constructor because AutoMapper uses the parameterless constructor.

--- backend/Contact.ify.Domain/DTOs/Contacts/CreateContactRequest.cs
+++ backend/Contact.ify.Domain/DTOs/Contacts/CreateContactRequest.cs
@@ -1,21 +1,18 @@
 using System.ComponentModel.DataAnnotations;
+using Contact.ify.Domain.DTOs.Emails;
 
 namespace Contact.ify.Domain.DTOs.Contacts;
 
 public class CreateContactRequest
 {
+    [Required] public string UserId { get; set; } = default!;
+        
     [MinLength(2, ErrorMessage = "First name must be at least 2 characters long")]
     [MaxLength(35, ErrorMessage = "First name cannot be longer than 35 characters")]
     public string? FirstName { get; set; }
-    
+
     [Required(ErrorMessage = "Last name is required")]
     [MinLength(2, ErrorMessage = "Last name must be at least 2 characters long")]
     [MaxLength(35, ErrorMessage = "Last name cannot be longer than 35 characters")]
-    public string LastName { get; set; }
-
-    public CreateContactRequest(string? firstName, string lastName)
-    {
-        FirstName = firstName;
-        LastName = lastName;
-    }
+    public string LastName { get; set; } = string.Empty;
 }
