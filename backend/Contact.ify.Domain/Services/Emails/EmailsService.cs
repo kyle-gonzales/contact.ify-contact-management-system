b1b28625b835ce74f703c894d8ebe4bd1913cfb4
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-23 22:48:46 +0800

    add address validation in UpdateEmailForContactAsync
    the check if the email exists is moved out of the repository and moved into the service instead.
    
    email is renamed to targetEmail for clarity since the parameters to the repository method are modified.

--- backend/Contact.ify.Domain/Services/Emails/EmailsService.cs
+++ backend/Contact.ify.Domain/Services/Emails/EmailsService.cs
@@ -29,28 +29,35 @@ public class EmailsService : IEmailsService
         _unitOfWork.Emails.AddEmail(email);
         
         contact.LastDateModified = DateTimeOffset.Now;
-        
         _unitOfWork.AuditTrail.Add(contactId, userId, ModificationType.Update, PropertyUpdated.Emails);
         await _unitOfWork.CompleteAsync();
+
         return email.ContactEmailId;
     }
 
-    public async Task UpdateEmailForContactAsync(string userId, int contactId, UpdateEmailRequest request)
+    public async Task<bool> UpdateEmailForContactAsync(string userId, int contactId, UpdateEmailRequest request)
     {
         var contact = await _unitOfWork.Contacts.GetContactByIdForUserAsync(userId, contactId);
         if (contact is null)
         {
-            return;
+            return false;
+        }
+        
+        var targetEmail = await _unitOfWork.Emails.GetEmailByIdForUserAsync(userId, contactId, request.ContactEmailId);
+        if (targetEmail is null) // email does not exist
+        {
+            return false;
         }
 
-        var email = _mapper.Map<ContactEmail>(request);
-        email.Contact = contact;
-        await _unitOfWork.Emails.UpdateEmailAsync(email);
+        var updatedEmail = _mapper.Map<ContactEmail>(request);
+        updatedEmail.Contact = contact;
+        _unitOfWork.Emails.UpdateEmail(targetEmail, updatedEmail);
 
         contact.LastDateModified = DateTimeOffset.UtcNow;
-    
         _unitOfWork.AuditTrail.Add(contactId, userId, ModificationType.Update, PropertyUpdated.Emails);
         await _unitOfWork.CompleteAsync();
+
+        return true;
     }
 
     public async Task DeleteEmailForUserAsync(string userId, int contactId, int id)
