32cd54a4e8908c5d4967e23f508deaa882e75028
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-08-25 01:12:15 +0800

    add UpdateUser method and refactor other methods in UsersRepository.cs

--- backend/Contact.ify.DataAccess/Repositories/Users/UsersRepository.cs
+++ backend/Contact.ify.DataAccess/Repositories/Users/UsersRepository.cs
@@ -13,16 +13,17 @@ public class UsersRepository : IUsersRepository
         _context = context;
     }
     
-    public void Add(User user)
+    public void AddUser(User user)
     {
         _context.Users.Add(user);
     }
 
-    public async Task<ICollection<User>> GetAllAsync()
+    public async Task<ICollection<User>> GetAllUsersAsync()
     {
         return await _context.Users.ToListAsync();
     }
     
+
     public async Task<bool> IsUserNameUniqueAsync(string userName)
     {
         return await _context.Users.CountAsync(user => user.UserId == userName) == 0;
@@ -33,7 +34,19 @@ public class UsersRepository : IUsersRepository
         return await _context.Users.AnyAsync(user => user.UserId == userName);
     }
 
-    public async Task<User?> GetAsync(string id)
+    public void UpdateUser(User targetUser, User updatedUser)
+    {
+        targetUser.FirstName = updatedUser.FirstName ?? targetUser.FirstName;
+        targetUser.LastName = updatedUser.LastName ?? targetUser.LastName;
+        targetUser.Email = updatedUser.Email ?? targetUser.Email;
+    }
+
+    public void UpdatePasswordForUser(User target, string passwordHash)
+    {
+        target.PasswordHash = passwordHash;
+    }
+
+    public async Task<User?> GetUserByUserNameAsync(string id)
     {
         return await _context.Users.FindAsync(id);
     }
