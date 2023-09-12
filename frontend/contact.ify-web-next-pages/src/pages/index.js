42dde799730c81394489004c141eaaa10e6c08be
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-09-12 16:25:25 +0800

    improve home page UI
    - wrapped the entire table in a Container
    - cursor is only set to pointer when hovering over isFavorite star

--- frontend/contact.ify-web-next-pages/src/pages/index.js
+++ frontend/contact.ify-web-next-pages/src/pages/index.js
@@ -4,7 +4,7 @@ import LoadingIndicator from "@/components/LoadingIndicator";
 import loadingStatus from "@/utils/loadingStatus";
 import Table from "react-bootstrap/Table";
 import useIsFavorite from "@/hooks/useIsFavorite";
-import { useMemo } from "react";
+import { Container } from "react-bootstrap";
 
 export default function Home() {
   const router = useRouter();
@@ -33,72 +33,84 @@ export default function Home() {
 
   return (
     <div>
-      <Table borderless={true} hover={true}>
-        <thead>
-          <tr>
-            <th>Last Name</th>
-            <th>First Name</th>
-          </tr>
-          <tr>
-            <td className="small text-muted" colSpan={3}>
-              Favorite Contacts ({favorites.length})
-            </td>
-          </tr>
-        </thead>
-        <tbody>
-          {favorites.map((contact) => (
-            <tr key={contact.id} style={{ cursor: "pointer" }}>
-              <td onClick={() => router.push(`/contacts/${contact.contactId}`)}>
-                {contact.lastName}
-              </td>
-              <td onClick={() => router.push(`/contacts/${contact.contactId}`)}>
-                {contact.firstName}
-              </td>
-              <td
-                onClick={() =>
-                  patchIsFavorite(`contacts/${contact.contactId}`, contact)
-                }
-              >
-                {contact.isFavorite ? (
-                  <i className="bi bi-star-fill" />
-                ) : (
-                  <i className="bi bi-star" />
-                )}
+      <Container>
+        <Table borderless={true} hover={true} responsive>
+          <thead>
+            <tr>
+              <th>Last Name</th>
+              <th>First Name</th>
+            </tr>
+            <tr>
+              <td className="small text-muted" colSpan={3}>
+                Favorite Contacts ({favorites.length})
               </td>
             </tr>
-          ))}
-        </tbody>
-        <thead>
-          <tr>
-            <td className="small text-muted" colSpan={3}>
-              Contacts ({contacts.length})
-            </td>
-          </tr>
-        </thead>
-        <tbody>
-          {contacts.map((contact) => (
-            <tr key={contact.id} style={{ cursor: "pointer" }}>
-              <td onClick={() => router.push(`/contacts/${contact.contactId}`)}>
-                {contact.lastName}
-              </td>
-              <td onClick={() => router.push(`/contacts/${contact.contactId}`)}>
-                {contact.firstName}
-              </td>
-              <td
-                onClick={() =>
-                  patchIsFavorite(`contacts/${contact.contactId}`, contact)
-                }
-              >
-                {contact.isFavorite ? (
-                  <i className="bi bi-star-fill" />
-                ) : (
-                  <i className="bi bi-star" />
-                )}
+          </thead>
+          <tbody>
+            {favorites.map((contact) => (
+              <tr key={contact.id} class="w-25">
+                <td
+                  onClick={() => router.push(`/contacts/${contact.contactId}`)}
+                >
+                  {contact.lastName}
+                </td>
+                <td
+                  onClick={() => router.push(`/contacts/${contact.contactId}`)}
+                >
+                  {contact.firstName}
+                </td>
+                <td
+                  onClick={() =>
+                    patchIsFavorite(`contacts/${contact.contactId}`, contact)
+                  }
+                  style={{ cursor: "pointer" }}
+                >
+                  {contact.isFavorite ? (
+                    <i className="bi bi-star-fill align-middle" />
+                  ) : (
+                    <i className="bi bi-star align-middle" />
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+          <thead>
+            <tr>
+              <td className="small text-muted" colSpan={3}>
+                Contacts ({contacts.length})
               </td>
             </tr>
-          ))}
-        </tbody>
-      </Table>
+          </thead>
+          <tbody>
+            {contacts.map((contact) => (
+              <tr key={contact.id}>
+                <td
+                  onClick={() => router.push(`/contacts/${contact.contactId}`)}
+                >
+                  {contact.lastName}
+                </td>
+                <td
+                  onClick={() => router.push(`/contacts/${contact.contactId}`)}
+                >
+                  {contact.firstName}
+                </td>
+                <td
+                  onClick={() =>
+                    patchIsFavorite(`contacts/${contact.contactId}`, contact)
+                  }
+                  style={{ cursor: "pointer" }}
+                >
+                  {contact.isFavorite ? (
+                    <i className="bi bi-star-fill align-middle" />
+                  ) : (
+                    <i className="bi bi-star " />
+                  )}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </Table>
+      </Container>
     </div>
   );
 }
