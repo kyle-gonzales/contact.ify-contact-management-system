3822674b9735ac7e56f5b592b52fe848d5f7f199
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-09-12 16:53:48 +0800

    feat: build Contactname component

---
+++ frontend/contact.ify-web-next-pages/src/components/ContactName.js
@@ -0,0 +1,37 @@
+import { useState } from "react";
+import { default as EditButton } from "./EditButton";
+
+const ContactName = ({ contact, patchIsFavorite }) => {
+  const [isHovered, setIsHovered] = useState(false);
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
+  return (
+    <div
+      className="d-flex mb-4 align-content-center justify-content-center"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <div
+        className="me-4 p-0"
+        onClick={() =>
+          patchIsFavorite(`contacts/${contact.contactId}`, contact)
+        }
+        style={{ cursor: "pointer" }}
+      >
+        {contact.isFavorite ? (
+          <i className="bi bi-star-fill align-middle" />
+        ) : (
+          <i className="bi bi-star align-middle" />
+        )}
+      </div>
+      <p className="h4 m-0">{contact.name}</p>
+      {isHovered && <EditButton />}
+    </div>
+  );
+};
+
+export default ContactName;
