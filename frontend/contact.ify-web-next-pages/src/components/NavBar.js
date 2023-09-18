be9f720b10bc75feef13ea161c66d7629a13f49b
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-09-19 00:03:33 +0800

    build: improve layout
    add side panel, add controlled state to search bar, and other UI fixes

--- frontend/contact.ify-web-next-pages/src/components/NavBar.js
+++ frontend/contact.ify-web-next-pages/src/components/NavBar.js
@@ -4,58 +4,85 @@ import Dropdown from "react-bootstrap/Dropdown";
 import CustomToggle from "./CustomToggle";
 import { useRouter } from "next/router";
 import cookies from "js-cookie";
-import useUser from "../hooks/useUser";
+import useUserToken from "../hooks/useUserToken";
+import { useState } from "react";
 
-const NavBar = () => {
+const NavBar = ({ togglePanel }) => {
   const router = useRouter();
-  const { user, setUser } = useUser(router);
+  const { user, setUser } = useUserToken(router);
+  const {
+    ["http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name"]: userName,
+  } = user ?? {};
   const logout = () => {
     setUser(null);
     cookies.remove("jwt_authorization");
-    router.reload();
+    router.replace("/auth/login");
+  };
+
+  const [searchInput, setSearchInput] = useState("");
+  const onValueChange = (e) => {
+    const { name, value } = e.target;
+    setSearchInput(value);
+  };
+
+  const search = (e) => {
+    e.preventDefault();
+    if (searchInput != null && searchInput.length !== 0) {
+      router.push(`/search/${searchInput}`);
+    }
   };
 
   return (
-    <nav className="navbar navbar-expand-md bg-dark">
+    <nav className="navbar navbar-expand-md bg-light sticky-top navbar-shadow mb-3">
       <div className="container-fluid">
-        <Link href="/">
-          <Image
-            src="/Contact.ify-white.svg"
-            height={50}
-            width={150}
-            alt="Logo"
+        <div className="d-flex align-items-center">
+          <i
+            className="bi bi-list me-2 icon-btn"
+            onClick={togglePanel}
+            style={{ fontSize: "24px" }}
           />
-        </Link>
-        {/* Search bar with responsive classes */}
-        <form className="d-grid col-4">
+          <Link href="/">
+            <Image
+              src="/Contact.ify-black.svg"
+              height={50}
+              width={150}
+              alt="Logo"
+            />
+          </Link>
+        </div>
+        {/* Search bar */}
+        <form className="d-grid col-4" onSubmit={search}>
           <div className="input-group">
             <span className="input-group-text bg-white p-0">
               <i
                 className="btn btn-outline-secondary border-0 bi bi-search btn-circle"
-                onClick={() => {
-                  alert("searching");
+                onClick={(e) => {
+                  search(e);
                 }}
               />
             </span>
             <input
               placeholder="Search"
-              className="form-control border-0"
+              className="form-control border"
               type="search"
+              value={searchInput}
+              onChange={onValueChange}
             />
           </div>
         </form>
         <div className="navbar-right">
           <Dropdown>
             <Dropdown.Toggle as={CustomToggle}>
-              <Image
-                src="/favicon.ico"
-                height={40}
-                width={40}
-                alt="User"
-                style={{ cursor: "pointer" }}
+              <i
+                className="bi bi-person-circle icon-btn"
+                style={{ fontSize: "30px", cursor: "pointer" }}
               />
             </Dropdown.Toggle>
-            <Dropdown.Menu>
+            <Dropdown.Menu className="my-shadow">
+              <small
+                className="d-block p-0 text-truncate text-uppercase px-3 text-light-gray"
+                style={{ maxWidth: "156px" }}
+              >{`Hi, ${userName}!`}</small>
               <Dropdown.Item href="/user">Profile</Dropdown.Item>
               <Dropdown.Divider />
               <Dropdown.Item className="text-danger" onClick={logout}>
--- frontend/contact.ify-web-next-pages/src/components/SidePanel.js
+++ frontend/contact.ify-web-next-pages/src/components/SidePanel.js
@@ -1,14 +1,69 @@
-import Link from "next/link"
+import Link from "next/link";
+import { useRouter } from "next/router";
+import { Button, Container, Row, Table } from "react-bootstrap";
 
-const SidePanel = () => {
+const SidePanel = ({ isOpen, togglePanel }) => {
+  const router = useRouter();
+
   return (
-    <div>
-      <Link href="/new">
-        <button>New Contact</button>
-      </Link>
-      <Link href="/favorites">
-        <button>Favorites</button>
-      </Link>
+    <div className={`side-panel ${isOpen ? "open" : ""}`} id="side-panel">
+      <div className="side-panel-content">
+        <Container>
+          <Row className="mb-3">
+            <Button
+              variant="outline-primary"
+              href="/new"
+              size="lg"
+              className=""
+              onClick={() => {
+                togglePanel();
+                router.push("/new");
+              }}
+            >
+              <div className="d-flex align-items-center">
+                <i className="bi bi-plus" style={{ fontSize: "24px" }} />
+                <span>New Contact</span>
+              </div>
+            </Button>
+          </Row>
+          <Row>
+            <Table borderless hover>
+              <tbody>
+                <tr>
+                  <td
+                    className="bg-light"
+                    onClick={() => {
+                      togglePanel();
+                      router.push("/");
+                    }}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <div className="d-flex align-items-center">
+                      <i className="bi bi-person-lines-fill me-2" />
+                      <span>Contacts</span>
+                    </div>
+                  </td>
+                </tr>
+                <tr>
+                  <td
+                    className="bg-light"
+                    onClick={() => {
+                      togglePanel();
+                      router.push("/favorites");
+                    }}
+                    style={{ cursor: "pointer" }}
+                  >
+                    <div className="d-flex align-items-center">
+                      <i className="bi bi-star-fill me-2" />
+                      <span>Favorites</span>
+                    </div>
+                  </td>
+                </tr>
+              </tbody>
+            </Table>
+          </Row>
+        </Container>
+      </div>
     </div>
   );
 };
