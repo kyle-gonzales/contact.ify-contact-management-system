fdfd0fc3405562c51b49f2253001486215bb5679
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-09-18 23:55:26 +0800

    feat: add dropdown for delete confirmation

--- frontend/contact.ify-web-next-pages/src/components/ContactModal.js
+++ frontend/contact.ify-web-next-pages/src/components/ContactModal.js
@@ -2,12 +2,14 @@ import formTypes from "@/utils/formId";
 import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
+import Dropdown from "react-bootstrap/Dropdown";
 
 function ContactModal({
   show,
   handleClose,
   isAdd = true,
   type = "",
+  deleteItem = () => {},
   children,
 }) {
   return (
@@ -33,24 +35,35 @@ function ContactModal({
               >
                 Cancel
               </Button>
-              <Button
-                variant="primary"
-                type="submit"
-                form={formTypes[type]}
-              >
+              <Button variant="primary" type="submit" form={formTypes[type]}>
                 Save
               </Button>
             </div>
           ) : (
             <div className="d-flex w-100 justify-content-between">
-              {type !== "Contact Name" && (
-                <div>
-                  <Button variant="outline-danger" onClick={()=>{}}>{/* TODO implement delete*/}
-                    {`Delete ${type}`}
-                  </Button>
-                </div>
-              )}
-              <div></div>
+              <div>
+                {/* <Button variant="outline-danger" onClick={deleteItem}>
+                  {`Delete ${type}`}
+                </Button> */}
+                <Dropdown>
+                  <Dropdown.Toggle variant="outline-danger" id="dropdown-basic">
+                    Delete
+                  </Dropdown.Toggle>
+
+                  <Dropdown.Menu className="my-shadow">
+                    <Dropdown.Item
+                      className="text-danger"
+                      onClick={(e) => {
+                        deleteItem(e);
+                      }}
+                    >
+                      Yes, Delete Me!
+                    </Dropdown.Item>
+                  </Dropdown.Menu>
+                </Dropdown>
+              </div>
+
+              <div></div> {/** this is needed */}
               <div>
                 <Button
                   variant="secondary"
@@ -59,11 +72,7 @@ function ContactModal({
                 >
                   Cancel
                 </Button>
-                <Button
-                  variant="primary"
-                  type="submit"
-                  form={formTypes[type]}
-                >
+                <Button variant="primary" type="submit" form={formTypes[type]}>
                   Save Changes
                 </Button>
               </div>
