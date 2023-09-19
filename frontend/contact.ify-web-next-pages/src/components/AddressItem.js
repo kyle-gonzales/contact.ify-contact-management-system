6750f6814dbcc4c6ad83cfa8ec192c4b481bf860
Kyle Angelo Gonzales <kyleangelog@gmail.com>
2023-09-19 14:35:39 +0800

    fix: display address type in AddressItem

--- frontend/contact.ify-web-next-pages/src/components/AddressItem.js
+++ frontend/contact.ify-web-next-pages/src/components/AddressItem.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import EditButton from "./EditButton";
 import Button from "react-bootstrap/Button";
+import addressTypes from "@/utils/addressTypes";
 
 const AddressItem = ({ address, onEditClicked }) => {
   const [isHovered, setIsHovered] = useState(false);
@@ -16,6 +17,9 @@ const AddressItem = ({ address, onEditClicked }) => {
       onMouseLeave={handleMouseLeave}
       className="d-flex align-items-center"
     >
+      <small className="text-muted text-uppercase me-2">
+        ({addressTypes[address.addressType - 1]})
+      </small>
       <span>{address.address}</span>
       {isHovered ? (
         <EditButton onEditClicked={onEditClicked} />
